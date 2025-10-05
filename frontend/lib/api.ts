fc14ccb7a5512b8d460eea8ca185b11a7ade3821
muhdfarhanaditi <muhammed.farhan@aditi-llc.com>
2025-10-05 16:30:42 +0530

    frontend url env change

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:500/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5001/api";
 
 // Create axios instance
 const ApiInstance = axios.create({
